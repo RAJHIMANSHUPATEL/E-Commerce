6390ba10e8e29b4ac7672cb3808107f15e802ae5
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2024-03-31 22:39:20 +0530

    display-price-in-featured-products

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,8 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+    return Intl.NumberFormat('en-us', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(number / 100)
+}
 
 export const getUniqueValues = () => {}
