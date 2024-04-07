f47900e4468da4b57c05876fa9a51d1c7b5ff8af
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2024-04-08 03:15:28 +0530

    review-stars

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -70,7 +70,10 @@ const SingleProductPage = () => {
           />
           <section className="content">
             <h2>{name}</h2>
-            <Stars />
+            <Stars
+              stars={stars}
+              reviews ={ reviews }
+            />
             <h5 className='price'>{formatPrice(price)}</h5>
             <p className='description'>{description}</p>
             <p className="info">
